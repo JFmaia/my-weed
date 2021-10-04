b7ae996e2677b373ab2af3a406c513e8f14c0dea
JFmaia <jfmaia741@gmail.com>
2021-10-04 10:18:52 -0300

    Ajustando nome da cor.

--- myweed/lib/modules/login/login_page.dart
+++ myweed/lib/modules/login/login_page.dart
@@ -7,7 +7,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.gradient_splash_primary,
+      backgroundColor: AppColors.primary,
       body: Column(),
     );
   }
