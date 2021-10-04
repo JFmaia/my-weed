c6bf807152901024808817369888edf157fe6e94
JFmaia <jfmaia741@gmail.com>
2021-10-04 09:37:19 -0300

    Criando pasta para as cores.

---
+++ myweed/lib/shared/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // ignore: non_constant_identifier_names
+  static final gradient_splash_primary = Color(0xFF0D0D0D);
+  // ignore: non_constant_identifier_names
+  static final gradient_splash_secundary = Color(0xFF4FA132);
+}
