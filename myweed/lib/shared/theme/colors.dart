590fe11d1c7e053eb564dbb5eede0220059869a3
JFmaia <jfmaia741@gmail.com>
2021-10-04 10:04:14 -0300

    Trocando cor primaria
    Foi visto que o gradiente usado não estava sendo iqual ao modelo do UI.

--- myweed/lib/shared/theme/colors.dart
+++ myweed/lib/shared/theme/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // ignore: non_constant_identifier_names
-  static final gradient_splash_primary = Color(0xFF0D0D0D);
+  static final gradient_splash_primary = Color(0xFF020B04);
   // ignore: non_constant_identifier_names
   static final gradient_splash_secundary = Color(0xFF4FA132);
 }
