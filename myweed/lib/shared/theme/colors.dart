a256a750bec6c06dcd7157d8f883975e15f691ab
JFmaia <jfmaia741@gmail.com>
2021-10-04 10:17:39 -0300

    Trocando nome da cor
    Trocando o nome da variavel para evitar erro de sintax.

--- myweed/lib/shared/theme/colors.dart
+++ myweed/lib/shared/theme/colors.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // ignore: non_constant_identifier_names
-  static final gradient_splash_primary = Color(0xFF020B04);
-  // ignore: non_constant_identifier_names
-  static final gradient_splash_secundary = Color(0xFF4FA132);
+  static final primary = Color(0xFF020B04);
+  static final secundary = Color(0xFF4FA132);
+  static final black = Color(0xFF0D0D0D);
 }
