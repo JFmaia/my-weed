b1b13c7018a280652b573cf764629297c6e7528c
JFmaia <jfmaia741@gmail.com>
2021-10-04 18:42:28 -0300

    Criando o layout do componente.
    Codificando o layout da caixa de entrada para ser efetuado o login.

--- myweed/lib/components/container_login/container_login.dart
+++ myweed/lib/components/container_login/container_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myweed/shared/theme/colors.dart';
 
 class ContainerLogin extends StatefulWidget {
   const ContainerLogin({Key? key}) : super(key: key);
@@ -11,8 +12,38 @@ class _ContainerLoginState extends State<ContainerLogin> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: AppColors.grey,
+      ),
       child: Column(
-        children: [],
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Color(0xFF020B04)),
+              ),
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Email or Phone number",
+                hintStyle: TextStyle(color: Colors.white54),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Password",
+                hintStyle: TextStyle(color: Colors.white54),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
