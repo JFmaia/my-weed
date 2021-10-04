a11b84203cf5be2f683c5529888d3712fda8a4e9
JFmaia <jfmaia741@gmail.com>
2021-10-04 16:48:03 -0300

    Criando o componente container_login
    Nesse component será feito a caixa com textos de email e senha para se logar.

---
+++ myweed/lib/components/container_login/container_login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ContainerLogin extends StatefulWidget {
+  const ContainerLogin({Key? key}) : super(key: key);
+
+  @override
+  _ContainerLoginState createState() => _ContainerLoginState();
+}
+
+class _ContainerLoginState extends State<ContainerLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
