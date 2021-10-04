c842e12490b3c0172dd686ee1deda946217c02ac
JFmaia <jfmaia741@gmail.com>
2021-10-04 18:43:39 -0300

    Trocando e ajustando as cores.

--- myweed/lib/shared/theme/colors.dart
+++ myweed/lib/shared/theme/colors.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static final primary = Color(0xFF020B04);
-  static final secundary = Color(0xFF4FA132);
+  static final secundary = Color(0xFF00EF00);
   static final black = Color(0xFF0D0D0D);
+  static final grey = Color(0xFF011105);
+  //021B08
+  //011105
 }
