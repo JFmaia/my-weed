fb8dd2a8057bba645adf5a609a73e490ad3e70b0
JFmaia <jfmaia741@gmail.com>
2021-10-06 07:48:43 -0300

    Criando um models
    Criando dentro de models o modelo de categorys.

---
+++ myweed/lib/shared/models/categorys.dart
@@ -0,0 +1,11 @@
+class Categorys {
+  final String id;
+  final String text;
+  final bool isSelected;
+
+  const Categorys({
+    required this.id,
+    required this.text,
+    required this.isSelected,
+  });
+}
