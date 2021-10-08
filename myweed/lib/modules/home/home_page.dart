1fdc62e31cc418c4a38a57c2a91fc4262736d7d0
JFmaia <jfmaia741@gmail.com>
2021-10-08 16:15:59 -0300

    Update na HomePage.
    Adicionado lista de produtos, com o layout feito.

--- myweed/lib/modules/home/home_page.dart
+++ myweed/lib/modules/home/home_page.dart
@@ -1,12 +1,17 @@
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:custom_fade_animation/custom_fade_animation.dart';
 import 'package:flutter/material.dart';
 import 'package:myweed/components/category/category.dart';
 import 'package:myweed/components/container_bar/container_bar.dart';
+import 'package:myweed/components/icon_grams/icon_grams.dart';
+import 'package:myweed/shared/theme/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,20 +19,114 @@ class HomePage extends StatelessWidget {
         preferredSize: Size.fromHeight(100),
         child: ContainerBar(),
       ),
-      body: Column(
-        children: [
-          Category(),
-          SizedBox(
-            height: 20,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Category(),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 10, left: 15),
+                height: 200,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 5),
+                        child: Container(
+                          width: 180,
+                          decoration: BoxDecoration(
+                            color: AppColors.grey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  top: 5,
+                                  bottom: 0,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Container(
+                                      height: 120,
+                                      width: 170,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                              "assets/images/hibrida.jpg"),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      child: IconGrams(quanty: 1),
+                                      bottom: 2,
+                                      right: 2,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  top: 8,
+                                  right: 2,
+                                  left: 10,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Sour Dive",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                        SizedBox(height: 4),
+                                        Text(
+                                          "R\$" + " 30,00",
+                                          style: TextStyle(
+                                            color: AppColors.secundary,
+                                            fontWeight: FontWeight.w300,
+                                            fontSize: 13,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Icons.star_border,
+                                        color: AppColors.secundary,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-          FadeAnimation(
-            1.5,
-            ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) => Container(),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
