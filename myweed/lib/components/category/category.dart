bc84f7a8df8a9293dd1a47bd835dc2a35547fee9
JFmaia <jfmaia741@gmail.com>
2021-10-08 16:14:03 -0300

    Retirando o FadeAnimation.

--- myweed/lib/components/category/category.dart
+++ myweed/lib/components/category/category.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:myweed/service/home/home_service.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:custom_fade_animation/custom_fade_animation.dart';
 import 'package:myweed/shared/theme/colors.dart';
 
 class Category extends StatefulWidget {
@@ -18,66 +16,60 @@ class _CategoryState extends State<Category> {
 
   @override
   Widget build(BuildContext context) {
-    return FadeAnimation(
-      1.2,
-      Container(
-        padding: EdgeInsets.only(top: 10, left: 15),
-        height: 45,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: _list.length,
-          itemBuilder: (BuildContext context, int index) {
-            return FadeAnimation(
-              0.8,
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _list[index].isSelected = !_list[index].isSelected;
-                  });
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 5),
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 300),
-                    width: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: _list[index].isSelected
-                          ? AppColors.secundary
-                          : Colors.transparent,
-                      border: Border.all(
-                        color: _list[index].isSelected
-                            ? Colors.transparent
-                            : AppColors.secundary,
-                        width: 1.5,
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(5),
-                          child: FittedBox(
-                            child: Text(
-                              _list[index].text,
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                color: _list[index].isSelected
-                                    ? AppColors.primary
-                                    : AppColors.secundary,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+    return Container(
+      padding: EdgeInsets.only(top: 10, left: 15),
+      height: 45,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: _list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                _list[index].isSelected = !_list[index].isSelected;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 5),
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                width: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: _list[index].isSelected
+                      ? AppColors.secundary
+                      : Colors.transparent,
+                  border: Border.all(
+                    color: _list[index].isSelected
+                        ? Colors.transparent
+                        : AppColors.secundary,
+                    width: 1.5,
                   ),
                 ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: FittedBox(
+                        child: Text(
+                          _list[index].text,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: _list[index].isSelected
+                                ? AppColors.primary
+                                : AppColors.secundary,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
