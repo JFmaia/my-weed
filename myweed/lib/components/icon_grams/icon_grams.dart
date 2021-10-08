03faa4611105183aae765f65e39064e59d35b6e9
JFmaia <jfmaia741@gmail.com>
2021-10-08 16:11:24 -0300

    Criando componente icon_grams.

---
+++ myweed/lib/components/icon_grams/icon_grams.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:myweed/shared/theme/colors.dart';
+
+class IconGrams extends StatelessWidget {
+  final int quanty;
+
+  const IconGrams({
+    Key? key,
+    required this.quanty,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30,
+      width: 30,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: AppColors.grey,
+          border: Border.all()),
+      child: Center(
+        child: Text(
+          "${this.quanty}" + "g",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
