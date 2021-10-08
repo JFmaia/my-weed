62be03905f7291f6d6f0dab23e8fb0ad78834ead
JFmaia <jfmaia741@gmail.com>
2021-10-08 16:24:15 -0300

    Refatorando uma parte do codigo.
    Uma parte do codigo foi refatorada e colocada no componente product_list.

--- myweed/lib/modules/home/home_page.dart
+++ myweed/lib/modules/home/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myweed/components/category/category.dart';
 import 'package:myweed/components/container_bar/container_bar.dart';
-import 'package:myweed/components/icon_grams/icon_grams.dart';
-import 'package:myweed/shared/theme/colors.dart';
+import 'package:myweed/components/product_list/product_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -34,93 +33,7 @@ class _HomePageState extends State<HomePage> {
                   scrollDirection: Axis.horizontal,
                   itemCount: 5,
                   itemBuilder: (BuildContext ctx, int index) {
-                    return GestureDetector(
-                      onTap: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 5),
-                        child: Container(
-                          width: 180,
-                          decoration: BoxDecoration(
-                            color: AppColors.grey,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  top: 5,
-                                  bottom: 0,
-                                ),
-                                child: Stack(
-                                  children: [
-                                    Container(
-                                      height: 120,
-                                      width: 170,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(5),
-                                        image: DecorationImage(
-                                          image: AssetImage(
-                                              "assets/images/hibrida.jpg"),
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                    ),
-                                    Positioned(
-                                      child: IconGrams(quanty: 1),
-                                      bottom: 2,
-                                      right: 2,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  top: 8,
-                                  right: 2,
-                                  left: 10,
-                                ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          "Sour Dive",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                        SizedBox(height: 4),
-                                        Text(
-                                          "R\$" + " 30,00",
-                                          style: TextStyle(
-                                            color: AppColors.secundary,
-                                            fontWeight: FontWeight.w300,
-                                            fontSize: 13,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Icons.star_border,
-                                        color: AppColors.secundary,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
+                    return ProductList();
                   },
                 ),
               ),
