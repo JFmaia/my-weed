faf6fdfdb25bcd5c00bc4d994596fa817ad683f0
JFmaia <jfmaia741@gmail.com>
2021-10-11 10:09:22 -0300

    Modificando Home para o bottomNavigator.

--- myweed/lib/modules/home/home_page.dart
+++ myweed/lib/modules/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:myweed/components/category/category.dart';
 import 'package:myweed/components/container_bar/container_bar.dart';
-import 'package:myweed/components/product_list/product_list.dart';
+import 'package:myweed/modules/favorites/favorites_page.dart';
+import 'package:myweed/modules/home_page/page_home.dart';
+import 'package:myweed/modules/profile/profile_page.dart';
+import 'package:myweed/modules/search/search_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,85 +20,24 @@ class _HomePageState extends State<HomePage> {
         preferredSize: Size.fromHeight(100),
         child: ContainerBar(),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              Category(),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 15,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "All Products",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 180,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 5,
-                        itemBuilder: (BuildContext ctx, int index) {
-                          return ProductList(
-                            imageUrl: "assets/images/hibrida.jpg",
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 15,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Products Rencet",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      height: 180,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 5,
-                        itemBuilder: (BuildContext ctx, int index) {
-                          return ProductList(
-                            imageUrl: "assets/images/sativa.jpg",
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+      body: [
+        PageHome(
+          key: UniqueKey(),
+        ),
+        SearchPage(
+          key: UniqueKey(),
+        ),
+        FavoritesPage(
+          key: UniqueKey(),
+        ),
+        ProfilePage(
+          key: UniqueKey(),
+        ),
+      ],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          children: [],
         ),
       ),
     );
