d9d5dd1ac6ffc4f84b32796f1d9dc7fc5cbaa1a4
JFmaia <jfmaia741@gmail.com>
2021-10-11 12:11:46 -0300

    Colocando um texto de Exemplo.

--- myweed/lib/modules/search/search_page.dart
+++ myweed/lib/modules/search/search_page.dart
@@ -5,6 +5,10 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Text("Busca"),
+      ),
+    );
   }
 }
