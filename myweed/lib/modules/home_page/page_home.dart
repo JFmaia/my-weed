7c6de872b07731c7902ba0394f2a329c0b25d9ac
JFmaia <jfmaia741@gmail.com>
2021-10-14 11:00:19 -0300

    Adicionando app bar a Home_page.

--- myweed/lib/modules/home_page/page_home.dart
+++ myweed/lib/modules/home_page/page_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myweed/components/category/category.dart';
+import 'package:myweed/components/container_bar/container_bar.dart';
 import 'package:myweed/components/product_list/product_list.dart';
 
 class PageHome extends StatefulWidget {
@@ -12,84 +13,90 @@ class PageHome extends StatefulWidget {
 class _PageHomeState extends State<PageHome> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        child: Column(
-          children: [
-            Category(),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 15,
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: ContainerBar(),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Category(),
+              SizedBox(
+                height: 20,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "All Products",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 15,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "All Products",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: 180,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 5,
-                      itemBuilder: (BuildContext ctx, int index) {
-                        return ProductList(
-                          imageUrl: "assets/images/hibrida.jpg",
-                        );
-                      },
+                    SizedBox(
+                      height: 10,
                     ),
-                  ),
-                ],
+                    Container(
+                      height: 180,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 5,
+                        itemBuilder: (BuildContext ctx, int index) {
+                          return ProductList(
+                            imageUrl: "assets/images/hibrida.jpg",
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 15,
+              SizedBox(
+                height: 20,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Products Rencet",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 15,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Products Rencet",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: 180,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 5,
-                      itemBuilder: (BuildContext ctx, int index) {
-                        return ProductList(
-                          imageUrl: "assets/images/sativa.jpg",
-                        );
-                      },
+                    SizedBox(
+                      height: 10,
                     ),
-                  ),
-                ],
+                    Container(
+                      height: 180,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 5,
+                        itemBuilder: (BuildContext ctx, int index) {
+                          return ProductList(
+                            imageUrl: "assets/images/sativa.jpg",
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
