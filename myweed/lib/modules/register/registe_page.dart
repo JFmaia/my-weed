8b9846a6d519e62dafe6aef2fd3b085841aa77bb
JFmaia <jfmaia741@gmail.com>
2021-10-18 11:15:23 -0300

    Criando o layout da pagina.

--- myweed/lib/modules/register/registe_page.dart
+++ myweed/lib/modules/register/registe_page.dart
@@ -27,6 +27,139 @@ class RegistePage extends StatelessWidget {
           },
         ),
       ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(
+            parent: ScrollPhysics(
+              parent: NeverScrollableScrollPhysics(),
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Crie sua conta agora!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 100),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: AppColors.grey,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Color(0xFF020B04)),
+                        ),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Nome",
+                          hintStyle: TextStyle(color: Colors.white54),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Color(0xFF020B04)),
+                        ),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Email ou celular",
+                          hintStyle: TextStyle(color: Colors.white54),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Color(0xFF020B04)),
+                        ),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "CPF",
+                          hintStyle: TextStyle(color: Colors.white54),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Data de nascimento",
+                          hintStyle: TextStyle(color: Colors.white54),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: 50,
+          color: Colors.transparent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // MaterialButton(
+              //   color: AppColors.secundary,
+              //   child: Text(
+              //     "Avançar",
+              //     style: TextStyle(
+              //       color: Colors.white,
+              //     ),
+              //   ),
+              //   onPressed: () {},
+              // ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 15,
+                ),
+                child: MaterialButton(
+                  height: 40,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  color: AppColors.secundary,
+                  child: Text(
+                    "Avançar",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
