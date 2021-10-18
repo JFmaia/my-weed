7d5ae9c702ec776686d2521102adc285a432972d
JFmaia <jfmaia741@gmail.com>
2021-10-18 10:12:35 -0300

    Modificando o navigator.
    Usando rotas nomeadas agora.

--- myweed/lib/modules/register/registe_page.dart
+++ myweed/lib/modules/register/registe_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:myweed/shared/theme/colors.dart';
 
 class RegistePage extends StatelessWidget {
   const RegistePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppColors.primary,
+        centerTitle: true,
+        title: Container(
+          height: 30,
+          width: 30,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/logo.png"),
+            ),
+          ),
+        ),
+        leading: BackButton(
+          color: AppColors.secundary,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
