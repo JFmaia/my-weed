adf0de248b302392ea7370f8d0a4e835f6b39f4f
JFmaia <jfmaia741@gmail.com>
2021-10-18 09:24:41 -0300

    Adicionando o main o MultiProvider.
    Multi provider serve para adicionar os controlles escolhidos em toda a arvore de contexto do programa.

--- myweed/lib/main.dart
+++ myweed/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:myweed/modules/home/home_controller.dart';
 import 'package:myweed/modules/splash/splash_page.dart';
 import 'package:myweed/shared/theme/colors.dart';
+import 'package:provider/provider.dart';
+
+import 'modules/login/login_controller.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +13,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: AppColors.primary,
-        canvasColor: AppColors.secundary,
+    return MultiProvider(
+      providers: [
+        Provider<HomeController>(
+          create: (_) => HomeController(),
+        ),
+        Provider<LoginController>(
+          create: (_) => LoginController(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: AppColors.primary,
+          canvasColor: AppColors.secundary,
+        ),
+        home: SplashPage(),
       ),
-      home: SplashPage(),
     );
   }
 }
