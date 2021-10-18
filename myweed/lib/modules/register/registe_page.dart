dddfa14a38b9d2bfc02c4cf6aa88331dae6b405c
JFmaia <jfmaia741@gmail.com>
2021-10-18 09:47:10 -0300

    Criando pagina de cadastro.

---
+++ myweed/lib/modules/register/registe_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegistePage extends StatelessWidget {
+  const RegistePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
