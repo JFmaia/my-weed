830f880d14500f438cf5390aaf5f47a2c1dd9de6
JFmaia <jfmaia741@gmail.com>
2021-10-19 09:58:24 -0300

    Adicionando AppBar

--- myweed/lib/modules/register/password_page.dart
+++ myweed/lib/modules/register/password_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:myweed/shared/theme/colors.dart';
 
 class PassawordPage extends StatelessWidget {
   const PassawordPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppColors.primary,
+        leading: BackButton(
+          color: AppColors.secundary,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
