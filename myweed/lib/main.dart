93056379e407831232f235f068b64288fa70bf21
JFmaia <jfmaia741@gmail.com>
2021-10-20 11:48:08 -0300

    Adicionando um novo provider.
    Provider para o controller da page register.

--- myweed/lib/main.dart
+++ myweed/lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:myweed/modules/home/home_controller.dart';
 import 'package:myweed/modules/home/home_page.dart';
 import 'package:myweed/modules/login/login_page.dart';
+import 'package:myweed/modules/register/password_page.dart';
 import 'package:myweed/modules/register/registe_page.dart';
 import 'package:myweed/modules/splash/splash_page.dart';
 import 'package:myweed/shared/theme/colors.dart';
 import 'package:provider/provider.dart';
-
 import 'modules/login/login_controller.dart';
+import 'modules/register/register_controller.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,6 +25,9 @@ class MyApp extends StatelessWidget {
         Provider<LoginController>(
           create: (_) => LoginController(),
         ),
+        Provider<RegisterController>(
+          create: (_) => RegisterController(),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -38,6 +42,7 @@ class MyApp extends StatelessWidget {
           "/home": (context) => HomePage(),
           "/login": (context) => LoginPage(),
           "/registe": (context) => RegistePage(),
+          "/password": (context) => PassawordPage(),
         },
       ),
     );
