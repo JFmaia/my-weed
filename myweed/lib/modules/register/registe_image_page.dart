5cd7236a61eb424779922c3652434f9aec4fdf21
JFmaia <jfmaia741@gmail.com>
2021-11-11 09:08:11 -0300

    Criando o inicio do layout.

--- myweed/lib/modules/register/registe_image_page.dart
+++ myweed/lib/modules/register/registe_image_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:myweed/shared/theme/colors.dart';
 
 class RegisterImage extends StatefulWidget {
   const RegisterImage({Key? key}) : super(key: key);
@@ -10,6 +12,46 @@ class RegisterImage extends StatefulWidget {
 class _RegisterImageState extends State<RegisterImage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final _media = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppColors.primary,
+        leading: BackButton(
+          color: AppColors.secundary,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(
+          parent: ScrollPhysics(
+            parent: NeverScrollableScrollPhysics(),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Observer(
+              builder: (context) => Stack(
+                children: [
+                  Container(
+                    height: _media.height * 0.20,
+                    width: _media.width * 0.20,
+                    decoration: BoxDecoration(
+                      color: AppColors.grey,
+                      borderRadius: BorderRadius.circular(
+                        20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
