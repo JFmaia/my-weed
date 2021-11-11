6f56cd0c32663e50eac916da5a97f57e08518fef
JFmaia <jfmaia741@gmail.com>
2021-11-11 08:49:58 -0300

    Pagina de seleção da foto de perfil.

---
+++ myweed/lib/modules/register/registe_image_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterImage extends StatefulWidget {
+  const RegisterImage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterImageState createState() => _RegisterImageState();
+}
+
+class _RegisterImageState extends State<RegisterImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
