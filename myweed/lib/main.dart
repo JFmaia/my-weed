defccce1c45fe0886d6f42442ad923ef601b50e2
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-14 08:42:53 -0300

    Removendo o provider.
    Percebido que o provider não erá preciso.

--- myweed/lib/main.dart
+++ myweed/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:myweed/modules/home/home_controller.dart';
 import 'package:myweed/modules/home/home_page.dart';
 import 'package:myweed/modules/login/login_page.dart';
 import 'package:myweed/modules/register/password_page.dart';
@@ -7,9 +6,6 @@ import 'package:myweed/modules/register/registe_image_page.dart';
 import 'package:myweed/modules/register/registe_page.dart';
 import 'package:myweed/modules/splash/splash_page.dart';
 import 'package:myweed/shared/theme/colors.dart';
-import 'package:provider/provider.dart';
-import 'modules/login/login_controller.dart';
-import 'modules/register/register_controller.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,35 +14,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<HomeController>(
-          create: (_) => HomeController(),
-        ),
-        Provider<LoginController>(
-          create: (_) => LoginController(),
-        ),
-        Provider<RegisterController>(
-          create: (_) => RegisterController(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: AppColors.primary,
-          canvasColor: AppColors.secundary,
-        ),
-        initialRoute: "/",
-        routes: {
-          "/": (context) => SplashPage(),
-          "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage(),
-          "/registe": (context) => RegistePage(),
-          "/password": (context) => PassawordPage(),
-          "/registe_image": (context) => RegisterImage(),
-        },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: AppColors.primary,
+        canvasColor: AppColors.secundary,
       ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+        "/registe": (context) => RegistePage(),
+        "/password": (context) => PassawordPage(),
+        "/registe_image": (context) => RegisterImage(),
+      },
     );
   }
 }
