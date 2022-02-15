4443b04e06afe54a9523b6a24782187c998f0c14
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-15 08:29:39 -0300

    Tirando o size do container bar.
    Pois o PreferredSize já toma conta disso.

--- myweed/lib/shared/components/container_bar/container_bar.dart
+++ myweed/lib/shared/components/container_bar/container_bar.dart
@@ -7,88 +7,89 @@ class ContainerBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      decoration: BoxDecoration(
-        color: AppColors.grey,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(20),
-          bottomRight: Radius.circular(20),
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.grey,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 25,
-          horizontal: 8,
-        ),
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage("assets/images/person.png"),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 8,
           ),
-          title: Text.rich(
-            TextSpan(
-              text: "Olá,",
-              children: [
-                TextSpan(
-                  text: " Person",
-                  style: TextStyle(
-                    color: AppColors.secundary,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-              ],
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/images/person.png"),
             ),
-          ),
-          subtitle: Text(
-            "Aproveite nossos serviços!",
-            style: TextStyle(
-              color: Colors.grey.shade300,
-              fontSize: 14,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          trailing: InkWell(
-            onTap: () {},
-            child: Stack(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 8,
-                  ),
-                  child: Container(
-                    height: 28,
-                    width: 28,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/marijuana.png"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  right: 18,
-                  child: Container(
-                    height: 15,
-                    width: 15,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
+            title: Text.rich(
+              TextSpan(
+                text: "Olá,",
+                children: [
+                  TextSpan(
+                    text: " Person",
+                    style: TextStyle(
                       color: AppColors.secundary,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
                     ),
-                    child: Center(
-                      child: Text(
-                        "1",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
+                  ),
+                ],
+              ),
+            ),
+            subtitle: Text(
+              "Aproveite nossos serviços!",
+              style: TextStyle(
+                color: Colors.grey.shade300,
+                fontSize: 14,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            trailing: InkWell(
+              onTap: () {},
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 8,
+                    ),
+                    child: Container(
+                      height: 28,
+                      width: 28,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/marijuana.png"),
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Positioned(
+                    right: 18,
+                    child: Container(
+                      height: 15,
+                      width: 15,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: AppColors.secundary,
+                      ),
+                      child: Center(
+                        child: Text(
+                          "1",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
