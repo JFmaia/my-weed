3d9f247d56fd078557500aa634f3bb8963babb37
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-15 06:58:58 -0300

    Deixando o main só com a chamada.

--- myweed/lib/main.dart
+++ myweed/lib/main.dart
@@ -1,34 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myweed/modules/home/home_page.dart';
-import 'package:myweed/modules/login/login_page.dart';
-import 'package:myweed/modules/register/password_page.dart';
-import 'package:myweed/modules/register/registe_image_page.dart';
-import 'package:myweed/modules/register/registe_page.dart';
-import 'package:myweed/modules/splash/splash_page.dart';
-import 'package:myweed/shared/theme/colors.dart';
+import 'package:myweed/app_widget.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: AppColors.primary,
-      ),
-      initialRoute: "/",
-      routes: {
-        "/": (context) => SplashPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
-        "/registe": (context) => RegistePage(),
-        "/password": (context) => PassawordPage(),
-        "/registe_image": (context) => RegisterImage(),
-      },
-    );
-  }
+  runApp(AppWidget());
 }
