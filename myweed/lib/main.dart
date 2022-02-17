acd4fab0b2f7ea508e4652e92f443922affe193a
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-17 20:28:52 -0300

    Modificações do Main.
    Foi retirada a inicialização do firebase e também foi feito o vinculo com o modular.

--- myweed/lib/main.dart
+++ myweed/lib/main.dart
@@ -1,43 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:myweed/app_module.dart';
 import 'package:myweed/app_widget.dart';
 
 void main() {
-  runApp(AppFirebase());
-}
-
-class AppFirebase extends StatefulWidget {
-  @override
-  _AppFirebaseState createState() => _AppFirebaseState();
-}
-
-class _AppFirebaseState extends State<AppFirebase> {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Material(
-            child: Center(
-              child: Text(
-                "Não é possível inicializar o Firebase",
-                textDirection: TextDirection.ltr,
-              ),
-            ),
-          );
-        } else if (snapshot.connectionState == ConnectionState.done) {
-          return AppWidget();
-        } else {
-          return Material(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-      },
-    );
-  }
+  return runApp(
+    ModularApp(
+      module: AppModule(),
+      child: AppWidget(),
+    ),
+  );
 }
