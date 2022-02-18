781e1c5ec48f1a482df5a42a62cbc27d3fa8c9a2
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-18 13:03:15 -0300

    Mudando o retorno do metodo.

--- myweed/lib/shared/service/app_database.dart
+++ myweed/lib/shared/service/app_database.dart
@@ -1,6 +1,7 @@
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:myweed/shared/service/firebase_database.dart';
 
 abstract class AppDatabase {
   static final AppDatabase instance = FirebaseDatabase();
-  Future<void> googleSignIn();
+  Future<GoogleSignInAccount?> googleSignIn();
 }
