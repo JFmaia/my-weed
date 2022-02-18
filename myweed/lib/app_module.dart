b3ca29864f1c15329c62d0a2082263c45c17338c
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-18 12:59:42 -0300

    Injetando dependencias.

--- myweed/lib/app_module.dart
+++ myweed/lib/app_module.dart
@@ -1,14 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:myweed/modules/home/home_page.dart';
+
 import 'package:myweed/modules/login/login_page.dart';
+import 'package:myweed/modules/login/repositories/login_repository.dart';
+import 'package:myweed/modules/login/repositories/login_repository_impl.dart';
 import 'package:myweed/modules/register/password_page.dart';
 import 'package:myweed/modules/register/registe_image_page.dart';
 import 'package:myweed/modules/register/registe_page.dart';
 import 'package:myweed/modules/splash/splash_page.dart';
+import 'package:myweed/shared/service/app_database.dart';
+import 'package:myweed/shared/service/firebase_database.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind<AppDatabase>((i) => FirebaseDatabase()),
+        Bind<LoginRepository>((i) => LoginRepositoryImpl()),
+      ];
 
   @override
   List<ModularRoute> get routes => [
