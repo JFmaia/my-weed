b3ca0f15b9f4b61e4355ebb72290e535cd238a83
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-21 15:26:13 -0300

    Criando novos metodos.
    Foi criado novos metodos para salvar o usuario logado e verificar se já há um usuario logado.

--- myweed/lib/shared/auth/auth_controller.dart
+++ myweed/lib/shared/auth/auth_controller.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:myweed/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-  var _isAuthenticated = false;
-  var _user;
+  UserModel? _user;
 
-  get user => _user;
+  UserModel get user => _user!;
 
-  void setUser(BuildContext context, var user) {
+  void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
+      saveUser(user);
       _user = user;
-      _isAuthenticated = true;
       Modular.to.pushReplacementNamed('/home');
     } else {
-      _isAuthenticated = false;
       Modular.to.pushReplacementNamed('/login');
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    if (instance.containsKey("user")) {
+      final json = instance.get("user") as String;
+      setUser(context, UserModel.fromJson(json));
+      return;
+    } else {
+      setUser(context, null);
     }
   }
 }
