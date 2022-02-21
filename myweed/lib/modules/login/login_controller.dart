7f07e7ce3773d20c3ee825fd74291773a73ab512
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-21 15:23:10 -0300

    Ajustando o loginController.
    Foi feitas mudanças e depois voltou ao que era antes, pois foi visto que era melhor do modo de antes.

--- myweed/lib/modules/login/login_controller.dart
+++ myweed/lib/modules/login/login_controller.dart
@@ -3,7 +3,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:myweed/modules/login/repositories/login_repository.dart';
 import 'package:myweed/shared/auth/auth_controller.dart';
-
+import 'package:myweed/shared/models/user_model.dart';
 part 'login_controller.g.dart';
 
 class LoginController = _LoginController with _$LoginController;
@@ -26,7 +26,11 @@ abstract class _LoginController with Store {
   Future<void> login(BuildContext context) async {
     try {
       final response = await repository.login();
-      authController.setUser(context, response);
+      final user = UserModel(
+        name: response!.displayName!,
+        imageURL: response.photoUrl,
+      );
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
