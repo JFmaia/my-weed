65c9b2cd1ad9ae1d114d320717d9707bc83ff3db
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-21 15:21:33 -0300

    Inicialização do firebase.
    Retornando a inicialização do Firebase para o main.

--- myweed/lib/main.dart
+++ myweed/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:myweed/app_module.dart';
@@ -7,7 +8,37 @@ void main() {
   return runApp(
     ModularApp(
       module: AppModule(),
-      child: AppWidget(),
+      child: AppFirebase(),
     ),
   );
+}
+
+class AppFirebase extends StatefulWidget {
+  @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                "Não é possível inicializar o Firebase",
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        } else {
+          return AppWidget();
+        }
+      },
+    );
+  }
 }
