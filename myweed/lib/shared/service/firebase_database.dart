fdaf3f6215bfc132ca6bb630fe3ec6c7f83f546d
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-23 11:47:10 -0300

    Criando novo metodo.
    Novo metodo que as classes que extende de database iram inplementar, metodo login por email e senha.

--- myweed/lib/shared/service/firebase_database.dart
+++ myweed/lib/shared/service/firebase_database.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:myweed/shared/service/app_database.dart';
 
@@ -15,5 +16,17 @@ class FirebaseDatabase implements AppDatabase {
     } else {
       return response;
     }
+  }
+
+  @override
+  Future<UserCredential> loginEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    final response = await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return response;
   }
 }
