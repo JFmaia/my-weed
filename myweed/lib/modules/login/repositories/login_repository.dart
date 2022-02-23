92d9f8eaf72b7fbe4061c6626d60d096f1674dee
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-23 11:51:16 -0300

    Criando novo metodo.
    Novo metodo que as classes que extende de login repositorio iram inplementar, metodo login por email e senha.

--- myweed/lib/modules/login/repositories/login_repository.dart
+++ myweed/lib/modules/login/repositories/login_repository.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class LoginRepository {
   Future<GoogleSignInAccount?> login();
+  Future<UserCredential?> loginEmailAndPassword(String email, String password);
 }
