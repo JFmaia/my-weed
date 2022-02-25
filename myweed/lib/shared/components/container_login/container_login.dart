ded3b81d5d7bb2b3aa4f54717d48d9f41eb65eaa
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-25 09:39:52 -0300

    Adicionando o Form.
    Com o form pode se criar uma key, que observa tudo dentro do form, assim podendo fazer uma validação sempre que submetido.

--- myweed/lib/shared/components/container_login/container_login.dart
+++ myweed/lib/shared/components/container_login/container_login.dart
@@ -15,58 +15,62 @@ class _ContainerLoginState extends State<ContainerLogin> {
   @override
   Widget build(BuildContext context) {
     final _controller = LoginController();
+    final _formkey = GlobalKey<FormState>();
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: AppColors.grey,
       ),
-      child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            decoration: const BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Color(0xFF020B04)),
+      child: Form(
+        key: _formkey,
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Color(0xFF020B04)),
+                ),
               ),
-            ),
-            child: TextFormField(
-              key: ValueKey("email"),
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "Email",
-                hintStyle: TextStyle(color: Colors.white54),
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Observer(
-              builder: (context) => TextFormField(
-                key: ValueKey("password"),
-                obscureText: _controller.isVisible,
+              child: TextFormField(
+                key: ValueKey("email"),
                 decoration: InputDecoration(
                   border: InputBorder.none,
-                  hintText: "Password",
+                  hintText: "Email",
                   hintStyle: TextStyle(color: Colors.white54),
-                  suffixIcon: GestureDetector(
-                    child: _controller.isVisible
-                        ? Icon(
-                            Icons.visibility_off,
-                            color: AppColors.secundary,
-                          )
-                        : Icon(
-                            Icons.visibility,
-                            color: AppColors.secundary,
-                          ),
-                    onTap: () {
-                      _controller.setVisible(!_controller.isVisible);
-                    },
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Observer(
+                builder: (context) => TextFormField(
+                  key: ValueKey("password"),
+                  obscureText: _controller.isVisible,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Password",
+                    hintStyle: TextStyle(color: Colors.white54),
+                    suffixIcon: GestureDetector(
+                      child: _controller.isVisible
+                          ? Icon(
+                              Icons.visibility_off,
+                              color: AppColors.secundary,
+                            )
+                          : Icon(
+                              Icons.visibility,
+                              color: AppColors.secundary,
+                            ),
+                      onTap: () {
+                        _controller.setVisible(!_controller.isVisible);
+                      },
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
