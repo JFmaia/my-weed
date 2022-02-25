210f4e9c2214eb3c3884f84926b74afff1df4107
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-25 15:39:10 -0300

    Atualizando.

--- myweed/lib/modules/login/login_page.dart
+++ myweed/lib/modules/login/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:myweed/modules/login/login_controller.dart';
-import 'package:myweed/shared/components/container_login/container_login.dart';
+import 'package:myweed/shared/components/button_login/button_login.dart';
 import 'package:myweed/shared/models/form_model.dart';
 import 'package:myweed/shared/theme/colors.dart';
 
@@ -15,17 +17,12 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final controller = LoginController();
-  Future<void> handleSubmit(FormModel dataForm) async {
-    final response = await controller.loginEmailAndPassword(
-      context,
-      dataForm.email,
-      dataForm.password,
-    );
-    if (response.isEmpty) {
-      return null;
-    } else {
-      _showError(response);
-    }
+  final _formkey = GlobalKey<FormState>();
+  final _formData = FormModel();
+
+  void _submit() {
+    final isValid = _formkey.currentState?.validate() ?? false;
+    if (!isValid) return;
   }
 
   void _showError(String erro) {
@@ -54,41 +51,193 @@ class _LoginPageState extends State<LoginPage> {
                 parent: NeverScrollableScrollPhysics(),
               ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 60,
-                    bottom: 20,
-                    left: 70,
-                  ),
-                  child: AnimatedCard(
-                    direction: AnimatedCardDirection.top,
-                    initDelay: Duration(microseconds: 5),
-                    curve: Curves.decelerate,
-                    child: Container(
-                      height: _media.height * 0.20,
-                      width: _media.width * 0.50,
-                      child: Image.asset(
-                        'assets/images/logo.png',
-                        filterQuality: FilterQuality.high,
-                        fit: BoxFit.cover,
+            child: Observer(
+              builder: (context) => Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 60,
+                      bottom: 20,
+                      left: 70,
+                    ),
+                    child: AnimatedCard(
+                      direction: AnimatedCardDirection.top,
+                      initDelay: Duration(microseconds: 5),
+                      curve: Curves.decelerate,
+                      child: Container(
+                        height: _media.height * 0.20,
+                        width: _media.width * 0.50,
+                        child: Image.asset(
+                          'assets/images/logo.png',
+                          filterQuality: FilterQuality.high,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                AnimatedCard(
-                  direction: AnimatedCardDirection.right,
-                  initDelay: Duration(microseconds: 1),
-                  child: ContainerLogin(
-                    onSubmit: handleSubmit,
+                  SizedBox(
+                    height: 30,
                   ),
-                ),
-              ],
+                  AnimatedCard(
+                    direction: AnimatedCardDirection.right,
+                    initDelay: Duration(microseconds: 1),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: AppColors.grey,
+                      ),
+                      child: Column(
+                        children: [
+                          Form(
+                            key: _formkey,
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 20),
+                                  decoration: const BoxDecoration(
+                                    border: Border(
+                                      bottom:
+                                          BorderSide(color: Color(0xFF020B04)),
+                                    ),
+                                  ),
+                                  child: TextFormField(
+                                    key: ValueKey("email"),
+                                    initialValue: _formData.email,
+                                    onChanged: (email) =>
+                                        _formData.email = email,
+                                    keyboardType: TextInputType.text,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Email",
+                                      hintStyle:
+                                          TextStyle(color: Colors.white54),
+                                    ),
+                                    validator: (_email) {
+                                      final email = _email ?? '';
+                                      if (!email.contains('@')) {
+                                        return "Email informado invalido!";
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 20),
+                                  child: TextFormField(
+                                    key: ValueKey("password"),
+                                    keyboardType: TextInputType.text,
+                                    initialValue: _formData.password,
+                                    obscureText: controller.isVisible,
+                                    onChanged: (password) =>
+                                        _formData.password = password,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Password",
+                                      hintStyle:
+                                          TextStyle(color: Colors.white54),
+                                      suffixIcon: GestureDetector(
+                                        child: controller.isVisible
+                                            ? Icon(
+                                                Icons.visibility_off,
+                                                color: AppColors.secundary,
+                                              )
+                                            : Icon(
+                                                Icons.visibility,
+                                                color: AppColors.secundary,
+                                              ),
+                                        onTap: () {
+                                          controller.setVisible(
+                                              !controller.isVisible);
+                                        },
+                                      ),
+                                    ),
+                                    validator: (_password) {
+                                      final password = _password ?? '';
+                                      if (password.length < 6) {
+                                        return "A senha deve ter no mínimo 6 caracteres.";
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      AnimatedCard(
+                        direction: AnimatedCardDirection.right,
+                        initDelay: Duration(microseconds: 1),
+                        child: TextButton(
+                          child: Text(
+                            "Esqueceu sua senha?",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 12),
+                  AnimatedCard(
+                    direction: AnimatedCardDirection.right,
+                    initDelay: Duration(microseconds: 1),
+                    child: ButtonPerson(
+                      text: "Entrar",
+                      corButton: AppColors.secundary,
+                      corText: Colors.white,
+                      onPressed: () {
+                        _submit();
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  AnimatedCard(
+                    direction: AnimatedCardDirection.right,
+                    initDelay: Duration(microseconds: 1),
+                    child: ButtonPerson(
+                      text: "Cadastre-se",
+                      corButton: AppColors.grey,
+                      corText: AppColors.secundary,
+                      onPressed: () =>
+                          Modular.to.pushReplacementNamed('/registe'),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  AnimatedCard(
+                    direction: AnimatedCardDirection.bottom,
+                    initDelay: Duration(microseconds: 1),
+                    child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      height: 50,
+                      minWidth: double.infinity,
+                      color: AppColors.grey,
+                      child: Container(
+                        height: 30,
+                        width: 30,
+                        child: Image(
+                          image: AssetImage("assets/images/google.png"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      onPressed: () {
+                        controller.loginGoogle(context);
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
