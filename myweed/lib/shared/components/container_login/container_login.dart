cc905220a3c56be0a1e5ddbe60344351e9ee81ce
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-25 12:47:46 -0300

    Colocando o observer.

--- myweed/lib/shared/components/container_login/container_login.dart
+++ myweed/lib/shared/components/container_login/container_login.dart
@@ -33,142 +33,145 @@ class _ContainerLoginState extends State<ContainerLogin> {
       widget.onSubmit(_formData);
     }
 
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: AppColors.grey,
-          ),
-          child: Column(
-            children: [
-              Form(
-                key: _formkey,
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 20),
-                      decoration: const BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(color: Color(0xFF020B04)),
-                        ),
-                      ),
-                      child: TextFormField(
-                        key: ValueKey("email"),
-                        initialValue: _formData.email,
-                        onChanged: (email) => _formData.email = email,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Email",
-                          hintStyle: TextStyle(color: Colors.white54),
-                        ),
-                        validator: (_email) => _controller.verfEmail(_email!),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 20),
-                      child: TextFormField(
-                        key: ValueKey("password"),
-                        initialValue: _formData.password,
-                        obscureText: _controller.isVisible,
-                        onChanged: (password) => _formData.password = password,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Password",
-                          hintStyle: TextStyle(color: Colors.white54),
-                          suffixIcon: GestureDetector(
-                            child: _controller.isVisible
-                                ? Icon(
-                                    Icons.visibility_off,
-                                    color: AppColors.secundary,
-                                  )
-                                : Icon(
-                                    Icons.visibility,
-                                    color: AppColors.secundary,
-                                  ),
-                            onTap: () {
-                              _controller.setVisible(!_controller.isVisible);
-                            },
+    return Observer(
+      builder: (context) => Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: AppColors.grey,
+            ),
+            child: Column(
+              children: [
+                Form(
+                  key: _formkey,
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Color(0xFF020B04)),
                           ),
                         ),
-                        validator: (_password) =>
-                            _controller.verfPassword(_password!),
+                        child: TextFormField(
+                          key: ValueKey("email"),
+                          initialValue: _formData.email,
+                          onChanged: (email) => _formData.email = email,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.white54),
+                          ),
+                          validator: (_email) => _controller.verfEmail(_email!),
+                        ),
                       ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        child: TextFormField(
+                          key: ValueKey("password"),
+                          initialValue: _formData.password,
+                          obscureText: _controller.isVisible,
+                          onChanged: (password) =>
+                              _formData.password = password,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.white54),
+                            suffixIcon: GestureDetector(
+                              child: _controller.isVisible
+                                  ? Icon(
+                                      Icons.visibility_off,
+                                      color: AppColors.secundary,
+                                    )
+                                  : Icon(
+                                      Icons.visibility,
+                                      color: AppColors.secundary,
+                                    ),
+                              onTap: () {
+                                _controller.setVisible(!_controller.isVisible);
+                              },
+                            ),
+                          ),
+                          validator: (_password) =>
+                              _controller.verfPassword(_password!),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              AnimatedCard(
+                direction: AnimatedCardDirection.right,
+                initDelay: Duration(microseconds: 1),
+                child: TextButton(
+                  child: Text(
+                    "Esqueceu sua senha?",
+                    style: TextStyle(
+                      color: Colors.white,
                     ),
-                  ],
+                  ),
+                  onPressed: () {},
                 ),
               ),
             ],
           ),
-        ),
-        Row(
-          children: [
-            AnimatedCard(
-              direction: AnimatedCardDirection.right,
+          SizedBox(height: 12),
+          AnimatedCard(
+            direction: AnimatedCardDirection.right,
+            initDelay: Duration(microseconds: 1),
+            child: ButtonPerson(
+              text: "Entrar",
+              corButton: AppColors.secundary,
+              corText: Colors.white,
+              onPressed: () => _submit,
+            ),
+          ),
+          SizedBox(height: 12),
+          AnimatedCard(
+            direction: AnimatedCardDirection.right,
+            initDelay: Duration(microseconds: 1),
+            child: ButtonPerson(
+              text: "Cadastre-se",
+              corButton: AppColors.grey,
+              corText: AppColors.secundary,
+              onPressed: () => Modular.to.pushReplacementNamed('/registe'),
+            ),
+          ),
+          SizedBox(height: 12),
+          Observer(
+            builder: (context) => AnimatedCard(
+              direction: AnimatedCardDirection.bottom,
               initDelay: Duration(microseconds: 1),
-              child: TextButton(
-                child: Text(
-                  "Esqueceu sua senha?",
-                  style: TextStyle(
-                    color: Colors.white,
+              child: MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                height: 50,
+                minWidth: double.infinity,
+                color: AppColors.grey,
+                child: Container(
+                  height: 30,
+                  width: 30,
+                  child: Image(
+                    image: AssetImage("assets/images/google.png"),
+                    fit: BoxFit.cover,
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  _controller.loginGoogle(context);
+                },
               ),
             ),
-          ],
-        ),
-        SizedBox(height: 12),
-        AnimatedCard(
-          direction: AnimatedCardDirection.right,
-          initDelay: Duration(microseconds: 1),
-          child: ButtonPerson(
-            text: "Entrar",
-            corButton: AppColors.secundary,
-            corText: Colors.white,
-            onPressed: () => _submit,
           ),
-        ),
-        SizedBox(height: 12),
-        AnimatedCard(
-          direction: AnimatedCardDirection.right,
-          initDelay: Duration(microseconds: 1),
-          child: ButtonPerson(
-            text: "Cadastre-se",
-            corButton: AppColors.grey,
-            corText: AppColors.secundary,
-            onPressed: () => Modular.to.pushReplacementNamed('/registe'),
-          ),
-        ),
-        SizedBox(height: 12),
-        Observer(
-          builder: (context) => AnimatedCard(
-            direction: AnimatedCardDirection.bottom,
-            initDelay: Duration(microseconds: 1),
-            child: MaterialButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              height: 50,
-              minWidth: double.infinity,
-              color: AppColors.grey,
-              child: Container(
-                height: 30,
-                width: 30,
-                child: Image(
-                  image: AssetImage("assets/images/google.png"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              onPressed: () {
-                _controller.loginGoogle(context);
-              },
-            ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
