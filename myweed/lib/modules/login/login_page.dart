5fc01ccddd5d7b61d46d7ae83f5d1e43e3f8bebb
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-25 12:16:02 -0300

    Modificando.

--- myweed/lib/modules/login/login_page.dart
+++ myweed/lib/modules/login/login_page.dart
@@ -1,12 +1,9 @@
 import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:myweed/modules/login/login_controller.dart';
-import 'package:myweed/shared/components/button_login/button_login.dart';
 import 'package:myweed/shared/components/container_login/container_login.dart';
 import 'package:myweed/shared/models/form_model.dart';
 import 'package:myweed/shared/theme/colors.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 /* Pagina de login, por onde é feita a autenticação de usuário, por email e senha ou google.*/
 class LoginPage extends StatefulWidget {
@@ -67,73 +64,6 @@ class _LoginPageState extends State<LoginPage> {
                     onSubmit: handleSubmit,
                   ),
                 ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    AnimatedCard(
-                      direction: AnimatedCardDirection.right,
-                      initDelay: Duration(microseconds: 1),
-                      child: TextButton(
-                        child: Text(
-                          "Esqueceu sua senha?",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 12),
-                AnimatedCard(
-                  direction: AnimatedCardDirection.right,
-                  initDelay: Duration(microseconds: 1),
-                  child: ButtonPerson(
-                    text: "Entrar",
-                    corButton: AppColors.secundary,
-                    corText: Colors.white,
-                    onPressed: () => Modular.to.pushReplacementNamed('/home'),
-                  ),
-                ),
-                SizedBox(height: 12),
-                AnimatedCard(
-                  direction: AnimatedCardDirection.right,
-                  initDelay: Duration(microseconds: 1),
-                  child: ButtonPerson(
-                    text: "Cadastre-se",
-                    corButton: AppColors.grey,
-                    corText: AppColors.secundary,
-                    onPressed: () =>
-                        Modular.to.pushReplacementNamed('/registe'),
-                  ),
-                ),
-                SizedBox(height: 12),
-                Observer(
-                  builder: (context) => AnimatedCard(
-                    direction: AnimatedCardDirection.bottom,
-                    initDelay: Duration(microseconds: 1),
-                    child: MaterialButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      height: 50,
-                      minWidth: double.infinity,
-                      color: AppColors.grey,
-                      child: Container(
-                        height: 30,
-                        width: 30,
-                        child: Image(
-                          image: AssetImage("assets/images/google.png"),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      onPressed: () {
-                        controller.loginGoogle(context);
-                      },
-                    ),
-                  ),
-                ),
               ],
             ),
           ),
@@ -142,3 +72,68 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+// Row(
+//             children: [
+//               AnimatedCard(
+//                 direction: AnimatedCardDirection.right,
+//                 initDelay: Duration(microseconds: 1),
+//                 child: TextButton(
+//                   child: Text(
+//                     "Esqueceu sua senha?",
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                   onPressed: () {},
+//                 ),
+//               ),
+//             ],
+//           ),
+//           SizedBox(height: 12),
+//           AnimatedCard(
+//             direction: AnimatedCardDirection.right,
+//             initDelay: Duration(microseconds: 1),
+//             child: ButtonPerson(
+//               text: "Entrar",
+//               corButton: AppColors.secundary,
+//               corText: Colors.white,
+//               onPressed: () => Modular.to.pushReplacementNamed('/home'),
+//             ),
+//           ),
+//           SizedBox(height: 12),
+//           AnimatedCard(
+//             direction: AnimatedCardDirection.right,
+//             initDelay: Duration(microseconds: 1),
+//             child: ButtonPerson(
+//               text: "Cadastre-se",
+//               corButton: AppColors.grey,
+//               corText: AppColors.secundary,
+//               onPressed: () => Modular.to.pushReplacementNamed('/registe'),
+//             ),
+//           ),
+//           SizedBox(height: 12),
+//           Observer(
+//             builder: (context) => AnimatedCard(
+//               direction: AnimatedCardDirection.bottom,
+//               initDelay: Duration(microseconds: 1),
+//               child: MaterialButton(
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(10),
+//                 ),
+//                 height: 50,
+//                 minWidth: double.infinity,
+//                 color: AppColors.grey,
+//                 child: Container(
+//                   height: 30,
+//                   width: 30,
+//                   child: Image(
+//                     image: AssetImage("assets/images/google.png"),
+//                     fit: BoxFit.cover,
+//                   ),
+//                 ),
+//                 onPressed: () {
+//                   _controller.loginGoogle(context);
+//                 },
+//               ),
+//             ),
+//           ),
