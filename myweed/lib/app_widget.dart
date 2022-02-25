193a0234bbb6227543e499810acfa5646cffba70
José Flávio S. Maia <jfmaia741@gmail.com>
2022-02-25 11:25:39 -0300

    Desativando a rotação.
    Com essa mudança foi retirada a rotação para horizontal do app.

--- myweed/lib/app_widget.dart
+++ myweed/lib/app_widget.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:myweed/shared/theme/colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitDown,
+        DeviceOrientation.portraitUp,
+      ],
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
